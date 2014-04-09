e8133f73c4b723e8242edc2d25132a82ee506921
Tobias Blaser <tobiasblaser@colvarim.ch>
2014-04-09 13:07:38 +0200

    Add caption inline editing

--- Configuration/angularConfig.js
+++ Configuration/angularConfig.js
@@ -28,6 +28,28 @@ define([
 		});
 	});
 
+	app.directive('contenteditable', function() {
+		return {
+			require: 'ngModel',
+			link: function(scope, element, attrs, ctrl) {
+				// view -> model
+				element.bind('blur', function() {
+					scope.$apply(function() {
+						ctrl.$setViewValue(element.html());
+					});
+				});
+
+				// model -> view
+				ctrl.$render = function() {
+					element.html(ctrl.$viewValue);
+				};
+
+				// load init value from DOM
+				ctrl.$render();
+			}
+		};
+	});
+
 
 	/* controllers */
 	app.controller('ImageController', ImageController);
