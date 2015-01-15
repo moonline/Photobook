e233dca92af8aa3f43eece79dbee4f04c13ae459
Tobias Blaser <tobiasblaser@colvarim.ch>
2015-01-15 14:47:24 +0100

    Refactoring: Move member function of image controller outside constructor

--- Classes/Application/ImageController.ts
+++ Classes/Application/ImageController.ts
@@ -12,70 +12,85 @@ module app.application {
 	'use strict';
 
 	export class ImageController {
-		$scope: any;
+		scope: any;
 
 		constructor($scope, $location) {
-			this.$scope = $scope;
+			this.scope = $scope;
 
-			this.$scope.title = "Image Manager";
-			this.$scope.photoBook = new app.domain.model.PhotoBook();
-			this.$scope.pages = [];
-			this.$scope.availableLayouts = Object.keys(configuration.LayoutConfiguration.layouts);
-			this.$scope.layouts = configuration.LayoutConfiguration.layouts;
-			this.$scope.pagesPerGroup = 4;
-			this.$scope.visiblePagesStart = 0;
+			this.scope.min = this.min.bind(this);
+			this.scope.isGroupStartPage = this.isGroupStartPage.bind(this);
+			this.scope.setVisiblePages = this.setVisiblePages.bind(this);
+			this.scope.getNumberList = this.getNumberList.bind(this);
+			this.scope.save = this.save.bind(this);
+			this.scope.loadFile = this.loadFile.bind(this);
 
-			$scope.min = function(num1: number, num2: number) {
-				return Math.min(num1, num2);
-			};
-
-			$scope.isGroupStartPage = function(page: number) {
-				return page % $scope.pagesPerGroup == 0;
-			};
-
-			$scope.setVisiblePages = function(startPage: number) {
-				$scope.visiblePagesStart = startPage;
-			};
-
-			$scope.getNumberList = function(start:number = 0, end:number = 10, step:number = 1) {
-				var numbers:number[] = [];
-				for(var i:number = start; i<=end ;i+=step) {
-					numbers.push(i);
-				}
-				return numbers;
-			};
-
-			$scope.save = function() {
-				while(!$scope.photoBook.title || $scope.photoBook.title == "") {
-					$scope.photoBook.title = prompt("Please enter the title of your photobook");
-				}
-				var serializedObjects: any[] = [];
-				var blob = new Blob([JSON.stringify(
-					$scope.photoBook,
-					function(key: any, value: any) {
-						// don't serialize parent relations of pages -> cyclic
-						if (key=="parentPhotoBook") {
-							return undefined;
-						}
-						else return value;
-					}
-				)], {type: 'application/json'});
-				var fileSaver = saveAs(blob,$scope.photoBook.title.replace(" ","-")+".json");
-			};
-
-			$scope.loadFile = function(files) {
-				app.service.FileService.readFile(files[0], function(fileContent) {
-					var photobook = new app.domain.model.PhotoBook();
-					photobook.importFromObject(JSON.parse(fileContent));
-					$scope.photoBook = photobook;
-					$scope.$apply();
-				});
-			};
+			this.scope.title = "Image Manager";
+			this.scope.photoBook = new app.domain.model.PhotoBook();
+			this.scope.pages = [];
+			this.scope.availableLayouts = Object.keys(configuration.LayoutConfiguration.layouts);
+			this.scope.layouts = configuration.LayoutConfiguration.layouts;
+			this.scope.pagesPerGroup = 4;
+			this.scope.visiblePagesStart = 0;
 
 			// prevent user from closing the browser accidentially
 			window.onbeforeunload = function() { return true; };
+		}
 
-			console.log($scope);
+
+		min(num1: number, num2: number): number {
+			return Math.min(num1, num2);
+		}
+
+		isGroupStartPage(page: number): boolean {
+			return page % this.scope.pagesPerGroup == 0;
+		}
+
+		setVisiblePages(startPage: number): void {
+			this.scope.visiblePagesStart = startPage;
+		}
+
+		getNumberList(start:number = 0, end:number = 10, step:number = 1): number[] {
+			var numbers:number[] = [];
+			for(var i:number = start; i<=end ;i+=step) {
+				numbers.push(i);
+			}
+			return numbers;
+		}
+
+		save(): void {
+			var scope = this.scope;
+
+			while(!this.scope.photoBook.title || this.scope.photoBook.title == "") {
+				this.scope.photoBook.title = prompt("Please enter the title of your photobook");
+			}
+			var serializedObjects: any[] = [];
+			var blob = new Blob([JSON.stringify(
+				scope.photoBook,
+				function(key: any, value: any) {
+					// don't serialize parent relations of pages -> cyclic
+					if (key=="parentPhotoBook") {
+						return undefined;
+					}
+					else return value;
+				}
+			)], {type: 'application/json'});
+			var fileSaver = saveAs(blob,this.scope.photoBook.title.replace(" ","-")+".json");
+		}
+
+		/**
+		 * Load content of a file
+		 *
+		 * @param files list of files from input type file
+		 */
+		loadFile(files) {
+			var scope = this.scope;
+
+			app.service.FileService.readFile(files[0], function(fileContent) {
+				var photobook = new app.domain.model.PhotoBook();
+				photobook.importFromObject(JSON.parse(fileContent));
+				scope.photoBook = photobook;
+				scope.$apply();
+			});
 		}
 	}
 }
