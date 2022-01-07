fe0747c7d7a923dea1902904a214758cba70d14f
Tobias Blaser <tblaser@logytsud.com.ar>
2022-01-07 10:36:15 -0300

    Remove experimental photobook viewer

--- photobook-viewer/src/domain/model/AbsolutePosition.ts
+++
@@ -1,9 +0,0 @@
-export class AbsolutePosition {
-    public readonly x: number;
-    public readonly y: number;
-
-    constructor(x: number = 0, y: number = 0) {
-        this.x = x;
-        this.y = y;
-    }
-}
